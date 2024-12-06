d4acfec9d16d5a1e2db61120ee61e2ca3c8a15ec
Alexmour0907 <mouritsenalexander@gmail.com>
2024-12-06 11:36:48 +0100

    fortsetter på JS. ikke ferdig

--- Bildegalleri/bildegalleri.js
+++ Bildegalleri/bildegalleri.js
@@ -1,8 +1,5 @@
 let bilde = document.getElementById("bilde");
 
-// bilde.src = "bilder/bil.jpg";
-
-
 
 let bildeGalleri = [
     "Bie på blomst.png",
@@ -25,14 +22,16 @@ function skiftBilde() {
 }
 
 let bildeGalleriMedTekst = [
-    { "bilde" : "TEST",            "bildeTekst" : "TEST NR1"},
-    { "bilde" : "Fjell_landskap.jpg", "bildeTekst" : "TEST NR2"}
+    { "bilde" : "TEST",   "bildeTekst" : "TEST NR1"},
+    { "bilde" : "TEST",   "bildeTekst" : "TEST NR2"}
 
 ];
 
 console.log(bildeGalleriMedTekst[0].bilde);
 console.log(bildeGalleriMedTekst[0].bildeTekst)
 console.log(bildeGalleriMedTekst[1].bildeTekst)
+
+
 
 
 
@@ -46,6 +45,17 @@ function nesteBilde() {
         bildeKilde = bilder[aktivtBilde]
         document.getElementById("bilde").src = bildeKilde;
         console.log(bildeKilde);
+    } else {
+        console.log("utenfor array.length")
+        aktivtBilde = 0;
+        document.getElementById("bilde").src = bilde[aktivtBilde]   
+    }
+}
+
+function forrigeBilde{
+    if (aktivtBilde > 0) {
+        aktivtBilde = aktivtBilde - 1;
+        bildeKilde = bilde
     } else {
         
     }
