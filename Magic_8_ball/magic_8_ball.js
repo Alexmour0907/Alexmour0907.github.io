c51e72873da6c06802c78c50115c179cb752846e
Alexmour0907 <mouritsenalexander@gmail.com>
2024-12-09 14:21:36 +0100

    Fikset at samme tall ikke kan komme 2 ganger på rad

--- Magic_8_ball/magic_8_ball.js
+++ Magic_8_ball/magic_8_ball.js
@@ -2,11 +2,18 @@
 
 listOfAwnsers = ["nei_8-ball", "JA_8-Ball", "Tror_ikke_8-ball", "Selfølge_8-ball", "aldri_8-ball"];
 
+let previousIndex = -1; // Variabel for å lagre forrige indeks
+
 document.getElementById("btnSpør").addEventListener("click", spør);
 
-function spør(){
-    let tilfeldig = Math.floor (Math.random() * listOfAwnsers.length);
-    console.log(tilfeldig)
+function spør() {
+    let tilfeldig;
+    do {
+        tilfeldig = Math.floor(Math.random() * listOfAwnsers.length);
+    } while (tilfeldig === previousIndex); // Sjekk om tallet er likt det forrige
+
+    previousIndex = tilfeldig; // Oppdater forrige indeks
+    console.log(tilfeldig);
     document.getElementById("bilde").src = "/Magic_8_ball/Bilder_til_magic8ball/SVG/" + listOfAwnsers[tilfeldig] + ".svg";
 }
 
