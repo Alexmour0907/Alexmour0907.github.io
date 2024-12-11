f086564029e8ac2156ffd5e3fd80490ea97adfa5
Alexmour0907 <mouritsenalexander@gmail.com>
2024-12-11 12:38:20 +0100

    nå fungerer det at knappen blir rød når den først trykkes på

--- reaksjon_test_spill/reaksjon.js
+++ reaksjon_test_spill/reaksjon.js
@@ -1,3 +1,11 @@
 let boks = document.getElementById("boks");
 
-boks.style.backgroundColor = red;
+boks.addEventListener("click", startTest);
+
+function startTest(){
+    boks.style.backgroundColor = "red";
+    boks.innerHTML = "Vent på grønt";
+    
+    const randomDelay = (Math.floor(Math.random() * 6) + 5) * 1000; // Random delay between 5 and 10 seconds
+    setInterval()
+}
